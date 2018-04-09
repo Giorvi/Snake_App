ca54b640f48576e5c4461e382826f8171defeb3a
Danielle Gamble <daniellehgamble@gmail.com>
2018-04-08 20:24:01 -0700

    Test commit

--- Snake.js
+++ Snake.js
@@ -27,6 +27,7 @@ function move(event) {
 
     snake.style.top = (snk.updown()) + "px";
     snake.style.left = (snk.leftright()) + "px";
+    // test
 }
 
 document.addEventListener('keydown', move);
