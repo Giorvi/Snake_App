623ca19e7d9c4f74c2914874eb3d3f22a0529c09
Danielle H Gamble <dg12181@srcs.loc>
2018-04-19 11:35:05 -0700

    first commit

--- Snake_Danielle.js
+++ Snake_Danielle.js
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
 var apple = getElementById("apple");
-var xpos = randInt()
-=======
->>>>>>> origin/master
+var xpos = randInt(0,500);
+var ypos = randInt(0,500);
+ apple.style.top = ypos + 'px';
+        apple.style.left = xpos + 'px';
