b8fc456644775d11aa1080661771f4d816cf796b
Danielle H Gamble <dg12181@srcs.loc>
2018-04-23 10:46:39 -0700

    working on apple collision

--- Snake.js
+++ Snake.js
@@ -37,15 +37,7 @@ function frame() {
     snake.style.top = ypos + 'px';
     snake.style.left = xpos + 'px'; 
 }
-   if (snake.x < apple.x + apple.width &&
-   snake.x + snake.width > apple.x &&
-   snake.y < apple.y + apple.height &&
-   snake.height + snake.y > apple.y) {
-    var yposition =  Math.floor(Math.random()*400) + 1;
-    var xposition = Math.floor(Math.random()*400) + 1;
-    apple.style.top = yposition + "px";
-    apple.style.left = xposition + "px";
-} 
+
 
 window.addEventListener("keypress", changeThingColor);
 
--- Snake_Danielle.js
+++ Snake_Danielle.js
@@ -3,3 +3,12 @@ var ypos =  Math.floor(Math.random()*400) + 1;
 var xpos = Math.floor(Math.random()*400) + 1;
 apple.style.top = ypos + "px";
 apple.style.left = xpos + "px";
+ if (snake.x < apple.x + apple.width &&
+   snake.x + snake.width > apple.x &&
+   snake.y < apple.y + apple.height &&
+   snake.height + snake.y > apple.y) {
+    var yposition =  Math.floor(Math.random()*400) + 1;
+    var xposition = Math.floor(Math.random()*400) + 1;
+    apple.style.top = yposition + "px";
+    apple.style.left = xposition + "px";
+}
