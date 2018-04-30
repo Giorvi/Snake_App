632b4b8277f983c3e16ef96cf55c5909adba4096
Giorvi Alvarez <giorvialvarez@giorvis-mbp.attlocal.net>
2018-04-29 21:12:56 -0700

    idk

--- Snake.js
+++ Snake.js
@@ -4,6 +4,7 @@
        span = document.getElementsByClassName("close")[0];
 
    function Snake(xpos, ypos, width, height, xstep, ystep, id) {
+
        this.xpos = xpos;
        this.ypos = ypos;
        this.width = width;
@@ -11,86 +12,100 @@
        this.xstep = xstep;
        this.ystep = ystep;
        this.id = id;
+
        var elem = document.getElementById(this.id);
+
        this.render = function () {
+
            elem.style.top = this.ypos + 'px';
            elem.style.left = this.xpos + 'px';
            elem.style.width = this.width + 'px';
            elem.style.height = this.height + 'px';
+
        }
 
        this.changeDirection = function (direction) {
+
            if (direction === "up") {
                this.ystep = -1;
+               this.ystep != 1;
                this.xstep = 0;
            }
            if (direction === "down") {
                this.ystep = 1;
+               this.ystep != -1;
                this.xstep = 0;
            }
            if (direction === "right") {
                this.xstep = 1;
+               this.xstep != -1;
                this.ystep = 0;
            }
            if (direction === "left") {
                this.xstep = -1;
+               this.xstep != 1;
                this.ystep = 0;
-           }
-           if (direction === "up"){
-
            }
 
        }
    }
 
    function Apple(appleX, appleY, width, height, appleid) {
+
        this.appleX = appleX;
        this.appleY = appleY;
        this.width = width;
        this.height = height;
        this.appleid = appleid;
+
        var elem1 = document.getElementById(this.appleid);
+
        this.render1 = function () {
            elem1.style.top = this.appleY + 'px';
            elem1.style.left = this.appleX + 'px';
            elem1.style.height = this.height + 'px';
            elem1.style.width = this.width + 'px';
+
        }
    }
 
-   var randomX = Math.floor(Math.random() * 350) + 1;
-   var randomY = Math.floor(Math.random() * 350) + 1;
+   var randomX = Math.floor(Math.random() * 350) + 1,
+       randomY = Math.floor(Math.random() * 350) + 1,
 
-   var snake = new Snake(55, 300, 25, 25, 0, 0, "snake");
-   var apple = new Apple(randomX, randomY, 25, 25, "apple");
+       snake = new Snake(55, 300, 25, 25, 0, 0, "snake"),
+       apple = new Apple(randomX, randomY, 25, 25, "apple"),
 
-
-   var id = setInterval(frame, 5);
+       id = setInterval(frame, 5);
 
    function frame() {
+
        apple.render1();
        checkCollideBorder();
        checkEatApple();
+
    }
 
-function addPoint () {
-    var startPoint = Number(document.getElementById('point').innerHTML);
-    startPoint++;
-            document.getElementById('point').innerHTML = score;
-
-}
+//   function addPoint() {
+//
+//       var startPoint = Number(document.getElementById('point').innerHTML);
+//       startPoint++;
+//       document.getElementById('point').innerHTML = score;
+//
+//   }
 
    function checkEatApple() {
+
        if (snake.xpos < apple.appleX + apple.width && snake.xpos + snake.width > apple.appleX && snake.ypos < apple.appleY + apple.height && snake.height + snake.ypos > apple.appleY) {
 
            apple.appleX = Math.floor(Math.random() * 350) + 1;
            apple.appleY = Math.floor(Math.random() * 350) + 1;
-
            addPoint();
+
        }
    }
 
    function checkCollideBorder() {
+
        if (snake.xpos > 375 || snake.xpos < 0) {
            snake.xstep = 0;
            snake.ystep = 0;
@@ -103,6 +118,7 @@ function addPoint () {
        snake.xpos = snake.xpos + snake.xstep;
        snake.ypos = snake.ypos + snake.ystep;
        snake.render();
+
    }
 
 
